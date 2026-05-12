db1c4bef5c623df5c35b02c266e4954857f54d37
Aman Sharma <139950825+amansharma2153@users.noreply.github.com>
2026-05-12 09:33:55 +0530

    Update axiosInstance.js

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,49 +1,7 @@
-// import axios from 'axios'
-
-// const API_URL = import.meta.env.VITE_API_URL
-
-// const axiosInstance = axios.create({
-//   baseURL: API_URL || 'http://localhost:5000/api',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// })
-
-// // 🔐 Add token
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token')
-
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-
-//     return config
-//   },
-//   (error) => Promise.reject(error)
-// )
-
-// // ❌ Handle errors
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     console.error("❌ AXIOS ERROR:", error.response?.data || error.message)
-
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token')
-//       window.location.href = '/'
-//     }
-
-//     return Promise.reject(error)
-//   }
-// )
-
-// export default axiosInstance
-
-
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = 'https://electronic-prescription-system-backend.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
